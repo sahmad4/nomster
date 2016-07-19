f111406924d9123fbf483ccbed7be8490f64307f
Salman Ahmad <salman_sv@yahoo.com>
2016-07-19 21:04:21 +0000

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ root 'places#index'
 resources :places do
   resources :comments, only: :create
 end
+resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
